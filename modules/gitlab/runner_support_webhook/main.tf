e8528ef875feb36d512357791cd227761315c7d8
Niklas van Schrick <mc.taucher2003@gmail.com>
2025-11-01 00:08:34 +0100

    Refactor runner support webhooks

---
+++ modules/gitlab/runner_support_webhook/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    gitlab = {
+      source = "gitlabhq/gitlab"
+      version = "18.5.0"
+    }
+  }
+}
+
+data "gitlab_project_variable" "runner_support_token" {
+  project = "code0-tech/secret-manager"
+  key     = "GITHUB_RUNNER_SUPPORT_TOKEN"
+}
+
+resource "gitlab_project_hook" "runner_support" {
+  project = var.project
+  url     = "https://api.github.com/repos/code0-tech/monoceros/actions/workflows/${var.runner_type}.yml/dispatches"
+
+  push_events = false
+  pipeline_events = true
+
+  custom_webhook_template = jsonencode(
+    {
+      "ref": "main",
+      "inputs": {
+        "project": "{{project.id}}"
+      }
+    }
+  )
+
+  custom_headers = [
+    {
+      key = "Authorization"
+      value = "Bearer ${data.gitlab_project_variable.runner_support_token.value}"
+    }
+  ]
+}
